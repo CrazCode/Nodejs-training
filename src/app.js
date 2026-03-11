3a4ab77a05417eae8a20a235d9506903caf076cc
CrazCode <crazcode0@gmail.com>
2026-03-11 16:12:37 +0300

    f

--- src/app.js
+++ src/app.js
@@ -1,32 +1,55 @@
 const express = require('express')
-const app = express()
-const PORT = 3000
+const mongoose = require('mongoose')
 
-const json = {
-  "name": "Mustafa",
-  "inc.": "faf",
-  "colors": [
-    "red", "blue", "green"
-  ],
-  "nums": [
-    1, 2, 3
-  ],
-  "people": [
-    {
-      "name": "mom",
-      "relationship": "parent"
-    },
-    {
-      "name": "dad",
-      "relationship": "parent"
-    },
-  ]
-}
+const app = express()
+mongoose.set('strictQuery', false);
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+process.env.NODE_ENV !== 'production' && require('dotenv').config();
+
+const PORT = process.env.PORT || 3000
+const CONNECTION = process.env.CONNECTION
+
+const customers = [
+  {
+    "name": "Mustafa",
+    "inc.": "facebook"
+  },
+  {
+    "name": "Manga",
+    "inc.": "instagram"
+  },
+  {
+    "name": "Tahsin",
+    "inc.": "yuotube"
+  },
+] 
 
 app.get('/', (req, res) => {
-  res.send(json)
+  res.send('Welcome to the Homepage!')
 })
 
-app.listen(PORT, () => {
-  console.log(`App is listening on ${PORT}!`)
+app.get('/api/customers', (req, res) => {
+  res.send({"customers": customers})
 })
+
+app.post('/api/customers', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
+
+const start = async() => {
+  try {
+    await mongoose.connect(CONNECTION)
+
+    app.listen(PORT, () => {
+      console.log(`App is listening on ${PORT}!`)
+    })
+  } catch(err) {
+    console.log(err.message)
+  }
+}
+
+start()
